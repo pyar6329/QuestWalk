5169e61378488a2c24db67420b9be633c247f386
Yusuke Ohashi <github@junkpiano.me>
2015-10-17 14:51:06 +0900

    Initial Commit

---
+++ QuestWalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuestWalk
+//
+//  Created by Ohashi Yusuke on 10/17/15.
+//  Copyright © 2015 SBR2015. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
