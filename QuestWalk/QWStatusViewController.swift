43f6734b137173c9b445241f4026c7b6fce745b1
shinjukujohnny <a13z3hk@aiit.ac.jp>
2015-10-17 17:06:25 +0900

    Add custom status View

---
+++ QuestWalk/QWStatusViewController.swift
@@ -0,0 +1,36 @@
+//
+//  QWStatusViewController.swift
+//  QuestWalk
+//
+//  Created by Johnny on 10/17/15.
+//  Copyright © 2015 SBR2015. All rights reserved.
+//
+
+import UIKit
+
+class QWStatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(NSStringFromClass(self.dynamicType))
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
